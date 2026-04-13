34c1dade43cad95bdd083af44355e601b6df4575
NextWaveDesigns <scurryshiquell@gmail.com>
2026-04-13 19:52:33 -0400

    Update page.tsx

--- src/app/workspace/page.tsx
+++ src/app/workspace/page.tsx
@@ -1,37 +1,23 @@
-+"use client";
-+
-+import
-{ useWorkspaceStore } from
-+
-../../s
-texport default function Workspace() {
-+
-const { prompt, setPrompt, generateAI,
-I
-+
-+
-+
-return (
-‹div style={{ padding: 20 }}>
-+
-<h2>NWD Workspace</h2>
-+
-+
-‹textarea
-+
-+
-+
-value={prompt}
-onChange={(e) = setPrompt(e.targe
-style={{ width: "100%", height: 10
-+
-+
-+
-+
-<button onClick={generateAI}>Generat
-+
-<pre>{response:</pre>
-+
-</div>
-+
-+}
+"use client";
+
+import { useWorkspaceStore } from "../../store/workspaceStore";
+
+export default function Workspace() {
+  const { prompt, setPrompt, generateAI, response } = useWorkspaceStore();
+
+  return (
+    <div style={{ padding: 20 }}>
+      <h2>NWD Workspace</h2>
+
+      <textarea
+        value={prompt}
+        onChange={(e) => setPrompt(e.target.value)}
+        style={{ width: "100%", height: 100 }}
+      />
+
+      <button onClick={generateAI}>Generate</button>
+
+      <pre>{response}</pre>
+    </div>
+  );
+}
