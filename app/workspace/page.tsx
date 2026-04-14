edadf1392e3628be1f5477a6b4665989a9a2e893
NextWaveDesigns <scurryshiquell@gmail.com>
2026-04-14 01:48:31 -0400

    Update page.tsx

--- app/workspace/page.tsx
+++ app/workspace/page.tsx
@@ -1,10 +1,5 @@
 "use client";
 
-import TopBar from "../../components/TopBar";
-import LeftPanel from "../../components/LeftPanel";
-import RightPanel from "../../components/RightPanel";
-import CanvasArea from "../../components/CanvasArea";
-
 export default function WorkspacePage() {
   return (
     <div className="workspace">
