bed9ee0ed6045acda881fd1bb8dc64df4d42b13d
NextWaveDesigns <scurryshiquell@gmail.com>
2026-04-14 07:54:02 -0400

    Create RightPanel.tsx

---
+++ app/components/RightPanel.tsx
@@ -0,0 +1,42 @@
+type Props = {
+  input: string;
+  setInput: (val: string) => void;
+  generate: () => void;
+  output: string;
+  loading: boolean;
+};
+
+export default function RightPanel({
+  input,
+  setInput,
+  generate,
+  output,
+  loading
+}: Props) {
+  return (
+    <div
+      style={{
+        width: 300,
+        background: "#0f172a",
+        color: "white",
+        padding: 15
+      }}
+    >
+      <h3>AI Designer</h3>
+
+      <textarea
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+        style={{ width: "100%", height: 100 }}
+      />
+
+      <button onClick={generate} style={{ marginTop: 10 }}>
+        {loading ? "Designing..." : "Generate"}
+      </button>
+
+      <div style={{ marginTop: 20, whiteSpace: "pre-wrap" }}>
+        {output}
+      </div>
+    </div>
+  );
+}
