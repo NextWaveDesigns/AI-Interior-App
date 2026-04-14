83440e1cc356c21cf1e7c3a704b8c61f446bdd4d
NextWaveDesigns <scurryshiquell@gmail.com>
2026-04-14 00:59:14 -0400

    Update page.tsx

--- app/workspace/page.tsx
+++ app/workspace/page.tsx
@@ -1,9 +1,9 @@
 "use client";
 
-import TopBar from "@/components/TopBar";
-import LeftPanel from "@/components/LeftPanel";
-import RightPanel from "@/components/RightPanel";
-import CanvasArea from "@/components/CanvasArea";
+import TopBar from "../../components/TopBar";
+import LeftPanel from "../../components/LeftPanel";
+import RightPanel from "../../components/RightPanel";
+import CanvasArea from "../../components/CanvasArea";
 
 export default function WorkspacePage() {
   return (
