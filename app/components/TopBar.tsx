5b6c865ceb147d161bc89c4aee39656ab9d639a7
NextWaveDesigns <scurryshiquell@gmail.com>
2026-04-14 07:53:05 -0400

    Create TopBar.tsx

---
+++ app/components/TopBar.tsx
@@ -0,0 +1,18 @@
+export default function TopBar() {
+  return (
+    <div
+      style={{
+        height: 60,
+        background: "#0f172a",
+        color: "white",
+        display: "flex",
+        alignItems: "center",
+        padding: "0 20px",
+        justifyContent: "space-between"
+      }}
+    >
+      <h2>NWD Design</h2>
+      <div>Workspace</div>
+    </div>
+  );
+}
